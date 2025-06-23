1917b9e8e65c2a7f645e628e9e790911a12d1b31
samuel-jimenez <53528911+samuel-jimenez@users.noreply.github.com>
2025-06-23 16:19:47 -0500

    chore: document PageMargin

--- wml/ctypes/pageMargin.go
+++ wml/ctypes/pageMargin.go
@@ -6,14 +6,22 @@ import (
 )
 
 // PageMargin represents the page margins of a Word document.
+// w_CT_PageMar =
 type PageMargin struct {
-	Left   *int `xml:"left,attr,omitempty"`
-	Right  *int `xml:"right,attr,omitempty"`
-	Gutter *int `xml:"gutter,attr,omitempty"`
-	Header *int `xml:"header,attr,omitempty"`
-	Top    *int `xml:"top,attr,omitempty"`
-	Footer *int `xml:"footer,attr,omitempty"`
+	// attribute w:top { w_ST_SignedTwipsMeasure },
+	Top *int `xml:"top,attr,omitempty"`
+	// attribute w:right { s_ST_TwipsMeasure },
+	Right *int `xml:"right,attr,omitempty"`
+	// attribute w:bottom { w_ST_SignedTwipsMeasure },
 	Bottom *int `xml:"bottom,attr,omitempty"`
+	// attribute w:left { s_ST_TwipsMeasure },
+	Left *int `xml:"left,attr,omitempty"`
+	// attribute w:header { s_ST_TwipsMeasure },
+	Header *int `xml:"header,attr,omitempty"`
+	// attribute w:footer { s_ST_TwipsMeasure },
+	Footer *int `xml:"footer,attr,omitempty"`
+	// attribute w:gutter { s_ST_TwipsMeasure }
+	Gutter *int `xml:"gutter,attr,omitempty"`
 }
 
 // MarshalXML implements the xml.Marshaler interface for the PageMargin type.
@@ -27,13 +35,13 @@ func (p PageMargin) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
 		local string
 		value *int
 	}{
-		{"w:left", p.Left},
-		{"w:right", p.Right},
-		{"w:gutter", p.Gutter},
-		{"w:header", p.Header},
 		{"w:top", p.Top},
-		{"w:footer", p.Footer},
+		{"w:right", p.Right},
 		{"w:bottom", p.Bottom},
+		{"w:left", p.Left},
+		{"w:header", p.Header},
+		{"w:footer", p.Footer},
+		{"w:gutter", p.Gutter},
 	}
 
 	for _, attr := range attrs {
